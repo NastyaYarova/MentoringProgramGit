1f8e6e6eea2262d9236dc52cb80cc90c634d1aff
anastasiia <anastasiia_yarova@epam.com>
2022-06-16 12:14:29 +0300

    First commit

--- src/main/java/GitExampleClass.java
+++ src/main/java/GitExampleClass.java
@@ -1,11 +1,11 @@
 public class GitExampleClass {
 
-    public int sum(int firstNumb, int secondNumb) {
-        return firstNumb + secondNumb;
+    public int sum(int firstNumb, int secondNumb, int thirdNumb) {
+        return firstNumb + secondNumb + thirdNumb;
     }
 
     public static void main(String[] args) {
         GitExampleClass gitExampleClass = new GitExampleClass();
-        System.out.println(gitExampleClass.sum(5, 6));
+        System.out.println(gitExampleClass.sum(5, 6, 7));
     }
 }
